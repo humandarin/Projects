1da3505444b09719149536523ec24b3621f605d4
Егор Алеев <93518428+humandarin@users.noreply.github.com>
2021-11-12 12:50:58 +0300

    Add files via upload

--- Taks 18 (short tic-tac-toe).cpp
+++ Taks 18 (short tic-tac-toe).cpp
@@ -8,6 +8,7 @@ int typegame;
 int sizeField;
 int firstMove;
 string doStart;
+string backMenuDefault;
 
 int goPlayTest1(int firstMove, int sizeField, int colorSymbX, int colorSymbO) {
 
@@ -358,20 +359,94 @@ int showSettings() {
 		cout << "[" << i << "] - " << "\x1b[" << i << "mHELLO\x1b[0m " << endl;
 	}*/
 
-	cout << "\x1b[104mЦвета: \x1b[0m\n";
-	cout << "\x1b[89m89 - классический;\n\x1b[90m90 - серый;\n\x1b[91m91 - красный;\n\x1b[92m92 - зеленый;\n";
-	cout << "\x1b[93m93 - желтый\n\x1b[94m94 - синий;\n\x1b[95m95 - фиолетовый;\n\x1b[96m96 - голубой;\n\x1b[97m97 - белый\n\n";
+	cout << "\x1b[104m<1>\x1b[0m Цвета: \n";
+	/*cout << "\x1b[89m89 - классический;\n\x1b[90m90 - серый;\n\x1b[91m91 - красный;\n\x1b[92m92 - зеленый;\n";
+	cout << "\x1b[93m93 - желтый\n\x1b[94m94 - синий;\n\x1b[95m95 - фиолетовый;\n\x1b[96m96 - голубой;\n\x1b[97m97 - белый\n\n";*/
 
-	cout << "\x1b[104mПервым ходит: \x1b[0m\n";
-	cout << "<1> Крестик \n";
-	cout << "<2> Нолик \n\n";
+	cout << "\x1b[104m<2>\x1b[0m Первым ходит: \n";
+	/*cout << "<1> Крестик \n";
+	cout << "<2> Нолик \n\n";*/
 
-	cout << "\x1b[104mРазмеры поля: \x1b[0m\n";
-	cout << "<1> 3x3 \n";
+	cout << "\x1b[104m<3>\x1b[0m Размеры поля: \n";
+	/*cout << "<1> 3x3 \n";
 	cout << "<2> 4x4 \n";
-	cout << "<3> 5x5 \n\n";
+	cout << "<3> 5x5 \n\n";*/
 
-	cout << "Выберите цвет крестика: ";
+	cout << "\x1b[104m<4>\x1b[0m Начать игру: \n";
+
+	cout << "\x1b[104m<5>\x1b[0m Выйти: \n";
+
+	int NumSetting;
+	cout << "Введите пункт меню: ";
+	cin >> NumSetting;
+
+	switch (NumSetting)
+	{
+	case 1: system("cls");
+		cout << "\x1b[104m<1>\x1b[0m Цвета: \n";
+		cout << "\x1b[89m89 - классический;\n\x1b[90m90 - серый;\n\x1b[91m91 - красный;\n\x1b[92m92 - зеленый;\n";
+		cout << "\x1b[93m93 - желтый\n\x1b[94m94 - синий;\n\x1b[95m95 - фиолетовый;\n\x1b[96m96 - голубой;\n\x1b[97m97 - белый\n\n";
+		cout << "Выберите цвет крестика: ";
+		cin >> colorSymbX;
+		if (colorSymbX > 97 || colorSymbX < 89) {
+			colorSymbX = rand() % 9 + 89;
+			cout << "Цвет крестика будет: " << "\x1b[" << colorSymbX << "m" << colorSymbX << "\x1b[0m\n";
+		}
+		cout << "Выберите цвет нолика: ";
+		cin >> colorSymbO;
+		if (colorSymbO > 97 || colorSymbO < 89) {
+			colorSymbO = rand() % 9 + 89;
+			cout << "Цвет нолика будет: " << "\x1b[" << colorSymbO << "m" << colorSymbO << "\x1b[0m\n";
+		}
+		showSettings();
+
+
+	case 2: system("cls");
+		cout << "\x1b[104m<2>\x1b[0m Первым ходит: \n";
+		cout << "<1> Крестик \n";
+		cout << "<2> Нолик \n\n";
+		cout << "\nКто ходит первый: ";
+		cin >> firstMove;
+		if (firstMove > 3) {
+			firstMove = rand() % 2 + 1;
+			cout << "Первым будет ходить: " << firstMove << "\x1b[0m\n";
+		}
+		showSettings();
+		break;
+
+	case 3: system("cls");
+		cout << "\x1b[104m<3>\x1b[0m Размеры поля: \n";
+		cout << "<1> 3x3 \n";
+		cout << "<2> 4x4 \n";
+		cout << "<3> 5x5 \n\n";
+		cout << "\nВыберите размер поля: ";
+		cin >> sizeField;
+		if (sizeField < 1 || sizeField > 3) {
+			sizeField = rand() % 3 + 1;
+			cout << "Размер поля будет: " << sizeField << "\x1b[0m\n";
+		}
+		showSettings();
+		break;
+
+	case 4:
+		cout << "Начать игрy (y/n)? ";
+		cin >> doStart;
+		if (doStart == "y") {
+			goPlayTest1(firstMove, sizeField, colorSymbX, colorSymbO);
+
+		}
+		break;
+
+	case 5:
+		cout << "\nВернуться в меню? (y/n) ";
+		cin >> backMenuDefault;
+		break;
+
+	default: goPlayTest1(firstMove, sizeField, colorSymbX, colorSymbO);
+		cout << "Начался классический тип игры: \n";
+	}
+
+	/*cout << "Выберите цвет крестика: ";
 	cin >> colorSymbX;
 	if (colorSymbX > 97 || colorSymbX < 89) {
 		colorSymbX = rand() % 9 + 89;
@@ -382,34 +457,23 @@ int showSettings() {
 	if (colorSymbO > 97 || colorSymbO < 89) {
 		colorSymbO = rand() % 9 + 89;
 		cout << "Цвет нолика будет: " << "\x1b[" << colorSymbO << "m" << colorSymbO << "\x1b[0m\n";
-	}
+	}*/
 
-	cout << "\nКто ходит первый: ";
+	/*cout << "\nКто ходит первый: ";
 	cin >> firstMove;
 	if (firstMove > 3) {
 		firstMove = rand() % 2 + 1;
 		cout << "Первым будет ходить: " << firstMove << "\x1b[0m\n";
-	}
+	}*/
 
-	cout << "\nВыберите размер поля: ";
+	/*cout << "\nВыберите размер поля: ";
 	cin >> sizeField;
 	if (sizeField < 1 || sizeField > 3) {
 		sizeField = rand() % 3 + 1;
 		cout << "Размер поля будет: " << sizeField << "\x1b[0m\n";
-	}
+	}*/
 
-	cout << "\nНачать игру (y/n)? ";
-	cin >> doStart;
 
-	if (doStart == "y") {
-		goPlayTest1(firstMove, sizeField, colorSymbX, colorSymbO);
-	}
-	else if (doStart == "n") {
-		cout << "Вы покинули игру :с";
-	}
-	else {
-		goPlayTest1(firstMove, sizeField, colorSymbX, colorSymbO);
-	}
 	/*switch (doStart) {
 	case 'y': goPlayTest(firstMove, sizeField, colorSymbX, colorSymbO);
 		break;
@@ -452,17 +516,22 @@ int showMenu() {
 
 	switch (numMenu) {
 	case 1:
-		/*sizeField = 1;
-		firstMove = 1;
-		colorSymbX = 91;
-		colorSymbO = 92;*/
 		goPlayTest1(1, 1, 91, 92);
 		if (typegame == 3) {
 			system("cls");
 			showMenu();
 		}
 		break;
-	case 2: showSettings();
+	case 2:
+		sizeField = 1;
+		firstMove = 1;
+		colorSymbX = 91;
+		colorSymbO = 92;
+		showSettings();
+		if (backMenuDefault == "y") {
+			system("cls");
+			showMenu();
+		}
 		if (doStart == "n") {
 			system("cls");
 			showMenu();
@@ -476,8 +545,6 @@ int showMenu() {
 	default: cout << "\nНет такого пункта меню(";
 		break;
 	}
-
-	//getSelect();
 
 	return 0;
 }
