ab7530a9fcdb83a7fc4c99bd166a20aa9270c634
Егор Алеев <93518428+humandarin@users.noreply.github.com>
2021-11-25 22:23:17 +0300

    Add files via upload

--- Task 19 (Quiz).cpp
+++ Task 19 (Quiz).cpp
@@ -9,17 +9,17 @@ int itemSettings;
 
 const int SIZE1 = 15;
 const int SIZE2 = 6;
-string arrayQuestions[SIZE1][SIZE2];
-
-int qtyQuestions = 15;
-int numQuestion[15]{ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 };
-int numAnswer[5]{ 0,1,2,3,4 };
-string numAnswerUser;
 
 int qtyLives = 5;
 int qtyHelps = 3;
 int qtyTime = 10;
 int qtyScores = 0;
+int qtyQuestions = 15;
+int numQuestion[15]{ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 };
+int numAnswer[5]{ 0,1,2,3,4 };
+
+int numAnswerUser;
+string arrayQuestions[SIZE1][SIZE2];
 
 
 //Викторина
@@ -35,105 +35,105 @@ int goQuiz() {
 		arrayQuestions[0][2] = "7,753 млрд";
 		arrayQuestions[0][3] = "126 млн";
 		arrayQuestions[0][4] = "16 тыс";
-		arrayQuestions[0][5] = "2";
+		arrayQuestions[0][5] = "7,753 млрд";
 
 		arrayQuestions[1][0] = "Какие пончики самые вкусные?";
 		arrayQuestions[1][1] = "С клубникой";
 		arrayQuestions[1][2] = "С шоколадом";
 		arrayQuestions[1][3] = "С корицей";
 		arrayQuestions[1][4] = "Пончики это миф";
-		arrayQuestions[1][5] = "2";
+		arrayQuestions[1][5] = "С шоколадом";
 
 		arrayQuestions[2][0] = "В какое время года едят хот-доги?";
 		arrayQuestions[2][1] = "Зимой вместе с глинтвейном";
 		arrayQuestions[2][2] = "Всегда";
 		arrayQuestions[2][3] = "На луне нет времен года";
 		arrayQuestions[2][4] = "Летом перед мороженым";
-		arrayQuestions[2][5] = "4";
+		arrayQuestions[2][5] = "Всегда";
 
 		arrayQuestions[3][0] = "Сколько типов машин в Яндекс.Такси?";
 		arrayQuestions[3][1] = "1";
 		arrayQuestions[3][2] = "6";
 		arrayQuestions[3][3] = "9";
 		arrayQuestions[3][4] = "14";
-		arrayQuestions[3][5] = "3";
+		arrayQuestions[3][5] = "9";
 
 		arrayQuestions[4][0] = "Как зовут четвертого из BTS?";
 		arrayQuestions[4][1] = "Джордж Вашингтон";
 		arrayQuestions[4][2] = "Ким Ки Дук";
 		arrayQuestions[4][3] = "RM";
 		arrayQuestions[4][4] = "Ким Чен Ын";
-		arrayQuestions[4][5] = "3";
+		arrayQuestions[4][5] = "RM";
 
 		arrayQuestions[5][0] = "Сколько максимум конфет можно съесть за время этой викторины?";
 		arrayQuestions[5][1] = "4";
 		arrayQuestions[5][2] = "12";
 		arrayQuestions[5][3] = "8";
 		arrayQuestions[5][4] = "Сахар это вредно";
-		arrayQuestions[5][5] = "3";
+		arrayQuestions[5][5] = "8";
 
 		arrayQuestions[6][0] = "Самый вкусный вкус Schwepps?";
 		arrayQuestions[6][1] = "Bitter Lemon";
 		arrayQuestions[6][2] = "Russchian Original";
 		arrayQuestions[6][3] = "Indian Tonic";
 		arrayQuestions[6][4] = "Spritz Aperetivo";
-		arrayQuestions[6][5] = "1";
+		arrayQuestions[6][5] = "Bitter Lemon";
 
 		arrayQuestions[7][0] = "Сколько времени лететь до Пакистана?";
 		arrayQuestions[7][1] = "10 часов";
 		arrayQuestions[7][2] = "5 минут";
 		arrayQuestions[7][3] = "18 часов";
 		arrayQuestions[7][4] = "6 часов";
-		arrayQuestions[7][5] = "1";
+		arrayQuestions[7][5] = "10 часов";
 
 		arrayQuestions[8][0] = "Почему самое вкусное мороженое в Икее?";
 		arrayQuestions[8][1] = "Потому что это Икеа";
 		arrayQuestions[8][2] = "В икее есть мороженое с облепихой";
 		arrayQuestions[8][3] = "Там лучшие фрикадельки";
 		arrayQuestions[8][4] = "У них хорошая мебель";
-		arrayQuestions[8][5] = "1";
+		arrayQuestions[8][5] = "Потому что это Икеа";
 
 		arrayQuestions[9][0] = "Сколько тысячелетий я писал эту викторину?";
 		arrayQuestions[9][1] = "12";
 		arrayQuestions[9][2] = "4";
 		arrayQuestions[9][3] = "0";
 		arrayQuestions[9][4] = "1";
-		arrayQuestions[9][5] = "3";
+		arrayQuestions[9][5] = "0";
 
 		arrayQuestions[10][0] = "Сколько обычно баллов трафика на дорогах в Москве в пятницу вечером?";
 		arrayQuestions[10][1] = "1-3";
 		arrayQuestions[10][2] = "4-5";
 		arrayQuestions[10][3] = "5-7";
 		arrayQuestions[10][4] = "8-10";
-		arrayQuestions[10][5] = "4";
+		arrayQuestions[10][5] = "8-10";
 
 		arrayQuestions[11][0] = "Какие грибы лучше не есть?";
 		arrayQuestions[11][1] = "Подосиновики";
 		arrayQuestions[11][2] = "Трюфели";
 		arrayQuestions[11][3] = "Мухоморы";
 		arrayQuestions[11][4] = "Белые грибы";
-		arrayQuestions[11][5] = "3";
+		arrayQuestions[11][5] = "Мухоморы";
 
 		arrayQuestions[12][0] = "Лучшая бургерная в Москве?";
 		arrayQuestions[12][1] = "Farш";
 		arrayQuestions[12][2] = "BB & Burgers";
 		arrayQuestions[12][3] = "Burger Heroes";
 		arrayQuestions[12][4] = "BEVERLY HILLS DINER";
-		arrayQuestions[12][5] = "3";
+		arrayQuestions[12][5] = "Burger Heroes";
 
 		arrayQuestions[13][0] = "2 * 2 + 2 * 2 + 8 = ?";
 		arrayQuestions[13][1] = "8";
 		arrayQuestions[13][2] = "44";
 		arrayQuestions[13][3] = "28";
 		arrayQuestions[13][4] = "16";
-		arrayQuestions[13][5] = "4";
+		arrayQuestions[13][5] = "16";
 
 		arrayQuestions[14][0] = "Какое сейчас время года?";
 		arrayQuestions[14][1] = "Зима";
 		arrayQuestions[14][2] = "Весна";
 		arrayQuestions[14][3] = "Лето";
 		arrayQuestions[14][4] = "Осень";
-		arrayQuestions[14][5] = "4";
+		arrayQuestions[14][5] = "Осень";
 	}
 
 	//Рандомирование вопросов
@@ -150,6 +150,7 @@ int goQuiz() {
 		}
 	}
 
+
 	//Рандомирование ответов
 	for (int i = 1; i < 5; i++) {
 		numAnswer[i] = rand() % 4 + 1;
@@ -164,39 +165,64 @@ int goQuiz() {
 		}
 	}
 
+	//for (int a = 0; a < 15; a++) {
+	//	cout << numQuestion[a] << "   ";
+	//}
+
+	string arrayQuestionsOther[SIZE1][SIZE2];
+
+	for (int k = 0; k < qtyQuestions; k++) {
+		arrayQuestionsOther[k][0] = arrayQuestions[numQuestion[k]][0];
+		arrayQuestionsOther[k][5] = arrayQuestions[numQuestion[k]][5];
+		for (int j = 1; j < 5; j++) {
+			arrayQuestionsOther[k][j] = arrayQuestions[numQuestion[k]][numAnswer[j]];
+		}
+	}
+
 	//Вывод вопросов и прием ответов
-	cout << "\x1b[90mВикторина началась!\x1b[0m\n";
-	cout << "У вас \x1b[90m" << qtyLives << " \x1b[0mжизней.\n\n";
+	cout << "\x1b[94mВикторина началась!\x1b[0m\n";
+	cout << "У вас \x1b[90m" << qtyLives << " \x1b[0mжизней.\n";
+	cout << "Для вывода подсказки введите \x1b[90m0\x1b[0m.\n\n\n";
 
 	for (int i = 0; i < qtyQuestions; i++) {
-		cout << "\x1b[90m<" << i + 1 << ">\x1b[0m " << arrayQuestions[numQuestion[i]][0] << endl << endl;
+		cout << "\x1b[90m<" << i+1 << ">\x1b[0m " << arrayQuestionsOther[i][0] << endl << endl;
 
 		for (int j = 1; j < 5; j++) {
-			cout << "\x1b[90m<" << numAnswer[j] << ">\x1b[0m " << arrayQuestions[numQuestion[i]][numAnswer[j]] << endl;
+			cout << "\x1b[90m<" << j << ">\x1b[0m " << arrayQuestionsOther[i][j] << endl;
 		}
-
-		/*Если пользователь указывает ячейку, в которой содержится то же, что и в
-		ячейке номер 5 - выводится Верно*/
 
 		cout << "\nВведите номер ответа: ";
 		cin >> numAnswerUser;
 
 		//Если вводится подсказка
-		if ((numAnswerUser == "0") && qtyLives >= 1) {
-			cout << "Один из правильных вариантов ответа: " << rand() % 5 + 1 << " или " << arrayQuestions[numQuestion[i]][5];
+		if ((numAnswerUser == 0) && qtyLives >= 1) {
+			int a = rand() % 2 + 1;
+			int random = rand() % 4 + 1;
+			if (arrayQuestionsOther[i][random] == arrayQuestionsOther[i][5]) {
+				while (arrayQuestionsOther[i][random] == arrayQuestionsOther[i][5]) {
+					random = rand() % 4 + 1;
+				}
+			}
+			if (a % 2 == 0) {
+				cout << "Один из правильных вариантов ответа: " << arrayQuestionsOther[i][random] << " или " << arrayQuestionsOther[i][5];
+			}
+			else {
+				cout << "Один из правильных вариантов ответа: " << arrayQuestionsOther[i][5] << " или " << arrayQuestionsOther[i][random];
+			}
 			qtyHelps--;
-			if (qtyHelps > 1) {
+			if (qtyHelps >= 1) {
 				cout << "\nПодсказок осталось: \x1b[94m" << qtyHelps << "\x1b[0m ";
 			}
 			else {
 				cout << "\nПодсказки закончились :с";
 			}
 			i--;
+			cout << "\n\n\x1b[94mОтветьте на вопрос еще раз ";
 		}
 
 		//Если вводится возможный вариант ответа
 		{
-			if ((numAnswerUser == arrayQuestions[numQuestion[i]][5]) && (qtyLives >= 1) && (numAnswerUser != "0")) {
+			if ((arrayQuestionsOther[i][numAnswerUser] == arrayQuestionsOther[i][5]) && (qtyLives >= 1) && (numAnswerUser != 0)) {
 				qtyScores++;
 				cout << "Верно!";
 				//Проверка на баллы
@@ -215,7 +241,7 @@ int goQuiz() {
 					}
 				}
 			}
-			else if ((numAnswerUser != arrayQuestions[numQuestion[i]][5]) && (qtyLives >= 1) && (numAnswerUser != "0")) {
+			else if ((arrayQuestionsOther[i][numAnswerUser] != arrayQuestionsOther[i][5]) && (qtyLives >= 1) && (numAnswerUser != 0)) {
 				qtyLives--;
 				cout << "Неверно :с";
 				//Проверка на жизни
@@ -234,7 +260,7 @@ int goQuiz() {
 					}
 				}
 			}
-			else if (numAnswerUser != "0") {
+			else if (numAnswerUser != 0) {
 				cout << "\n\n\x1b[91mВы проиграли. У вас кончились жизни :с\x1b[0m";
 				i = 100;
 			}
